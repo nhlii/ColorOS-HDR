93e06fe382808519d3934de6bd9a86cdd0686a08
nhlii <140586629+nhlii@users.noreply.github.com>
2025-05-18 20:54:22 +0800

    Add files via upload

---
+++ post-mount.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+
+# OverlayFS参数设置
+TARGET="/system/my_product/vendor/etc"    # 要覆盖的目录
+LOWER="/system/my_product/vendor/etc"     # 原系统目录
+UPPER="$MODDIR/system/my_product/vendor/etc"  # 你的修改文件所在目录
+WORK="$MODDIR/work"                       # 临时工作目录
+
+# 创建必要目录
+mkdir -p "$TARGET" "$UPPER" "$WORK"
+
+# 挂载OverlayFS（合并原目录和你的修改）
+mount -t overlay overlay -o lowerdir="$LOWER",upperdir="$UPPER",workdir="$WORK" "$TARGET"

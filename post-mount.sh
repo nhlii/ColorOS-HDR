5ff1c58cbe4cd7cc50c95f118e9434573c6d2cdf
nhlii <140586629+nhlii@users.noreply.github.com>
2025-05-20 11:33:53 +0800

    Update post-mount.sh

--- post-mount.sh
+++ post-mount.sh
@@ -2,13 +2,13 @@
 MODDIR=${0%/*}
 
 # OverlayFS参数设置
-LOWER="/system/my_product/vendor/etc"     # 下级目录
-UPPER="$MODDIR/system/my_product/vendor/etc"  # 上级目录
-WORK="$MODDIR/work"  # 临时工作目录
-MERGER="$MODDIR/merged"    # 挂载点
+LOWER_CHU="/system/my_product/vendor/etc"     # 下级目录
+UPPER_CHU="$MODDIR/system/my_product/vendor/etc"  # 上级目录
+WORK_CHU="$MODDIR/work"  # 临时工作目录
+MERGER_CHU="$MODDIR/merged"    # 挂载点
 
 # 创建必要目录
-mkdir -p "$UPPER" "$WORK" "$MERGER"
+mkdir -p "$UPPER_CHU" "$WORK_CHU" "$MERGER_CHU"
 
 # 挂载OverlayFS
-mount -t overlay KSU -o lowerdir="$LOWER",upperdir="$UPPER",workdir="$WORK" "$MERGER"
+mount -t overlay KSU -o lowerdir="$LOWER_CHU",upperdir="$UPPER_CHU",workdir="$WORK_CHU" "$MERGER_CHU"

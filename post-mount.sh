26f6237ee24a80cd17fbd1befbccb68f81cb6548
nhlii <140586629+nhlii@users.noreply.github.com>
2025-05-20 10:06:15 +0800

    Update post-mount.sh

--- post-mount.sh
+++ post-mount.sh
@@ -2,13 +2,13 @@
 MODDIR=${0%/*}
 
 # OverlayFS参数设置
-TARGET="/system/my_product/vendor/etc"    # 要覆盖的目录
-LOWER="/system/my_product/vendor/etc"     # 原系统目录
-UPPER="$MODDIR/system/my_product/vendor/etc"  # 你的修改文件所在目录
-WORK="$MODDIR/system/my_product/vendor/work"  # 临时工作目录
+MERGER="/system/my_product/vendor/etc"    # 挂载点
+LOWER="/system/my_product/vendor/etc"     # 下级目录
+UPPER="$MODDIR/system/my_product/vendor/etc"  # 上级目录
+WORK="$MODDIR/work"  # 临时工作目录
 
 # 创建必要目录
-mkdir -p "$TARGET" "$UPPER" "$WORK"
+mkdir -p "$MERGER" "$UPPER" "$WORK"
 
-# 挂载OverlayFS（合并原目录和你的修改）
-mount -t overlay KSU -o lowerdir="$LOWER",upperdir="$UPPER",workdir="$WORK" "$TARGET"
+# 挂载OverlayFS
+mount -t overlay KSU -o lowerdir="$LOWER",upperdir="$UPPER",workdir="$WORK" "$MERGER"
